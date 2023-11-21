0a649e80851281a7dcb63f09739c4b6417350648
chuxiwen <1609185446@qq.com>
2023-11-21 14:58:23 +0800

    继承抽象类实现单向循环链表逻辑

---
+++ src/main/java/com/liu/dynamic/singlecirclelinked/DynamicSingleCircleLinkedList.java
@@ -0,0 +1,119 @@
+package com.liu.dynamic.singlecirclelinked;
+
+import com.liu.dynamic.AbstractDynamicList;
+
+@SuppressWarnings("all")
+public class DynamicSingleCircleLinkedList<E> extends AbstractDynamicList<E> {
+
+    private Node<E> first;
+
+    @Override
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkIndexForAdd(index);
+        if (index == 0) {
+            Node<E> newFirst = new Node<>(element, first);
+            Node<E> last = (size == 0) ? newFirst : node(size - 1);
+            last.next = newFirst;
+            first = newFirst;
+        } else {
+            Node<E> prev = node(index - 1);
+            prev.next = new Node<>(element, prev.next);
+        }
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+        Node<E> node = first;
+        if (index == 0) {
+            if (size == 1) {
+                first = null;
+            } else {
+                Node<E> last = node(size - 1);
+                first = first.next;
+                last.next = first;
+            }
+        } else {
+            Node<E> prev = node(index - 1);
+            node = prev.next;
+            prev.next = node.next;
+        }
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        Node<E> node = first;
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (node.element == null) return i;
+                node = node.next;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) return i;
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        first = null;
+    }
+
+    private Node<E> node(int index) {
+        checkIndex(index);
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[size=").append(size).append(", ");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) builder.append(", ");
+            builder.append(node.element);
+            node = node.next;
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return element + "_" + next.element;
+        }
+    }
+}
