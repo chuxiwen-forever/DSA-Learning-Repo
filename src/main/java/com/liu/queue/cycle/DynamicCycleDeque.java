6384479b2527ff3b26193daeeb6079bac42c79ee
chuxiwen <1609185446@qq.com>
2023-11-23 15:54:33 +0800

    实现双向队列接口实现循环双向队列

--- src/main/java/com/liu/queue/cycle/DynamicCycleDeque.java
+++ src/main/java/com/liu/queue/cycle/DynamicCycleDeque.java
@@ -1,19 +1,16 @@
 package com.liu.queue.cycle;
 
-import com.liu.queue.DynamicQueue;
+import com.liu.queue.DynamicDeque;
 
 @SuppressWarnings("all")
-public class DynamicCycleQueue<E> implements DynamicQueue<E> {
+public class DynamicCycleDeque<E> implements DynamicDeque<E> {
 
     private int front;
-
     private int size;
-
     private E elements[];
+    private static final int DEFAULT_CAPACITY = 10;
 
-    public static final int DEFAULT_CAPACITY = 10;
-
-    public DynamicCycleQueue() {
+    public DynamicCycleDeque() {
         elements = (E[]) new Object[DEFAULT_CAPACITY];
     }
 
@@ -32,24 +29,41 @@ public class DynamicCycleQueue<E> implements DynamicQueue<E> {
         for (int i = 0; i < size; i++) {
             elements[index(i)] = null;
         }
-        size = 0;
         front = 0;
+        size = 0;
     }
 
     @Override
-    public void enQueue(E element) {
+    public void enQueueRear(E element) {
         ensureCapacity(size + 1);
         elements[index(size)] = element;
         size++;
     }
 
     @Override
-    public E deQueue() {
-        E fronElement = elements[front];
+    public E deQueueRear() {
+        int rearIndex = index(size - 1);
+        E rear = elements[rearIndex];
+        elements[rearIndex] = null;
+        size--;
+        return rear;
+    }
+
+    @Override
+    public void enQueueFront(E element) {
+        ensureCapacity(size + 1);
+        front = index(-1);
+        elements[front] = element;
+        size++;
+    }
+
+    @Override
+    public E deQueueFront() {
+        E frontElement = elements[front];
         elements[front] = null;
         front = index(1);
         size--;
-        return fronElement;
+        return frontElement;
     }
 
     @Override
@@ -57,11 +71,19 @@ public class DynamicCycleQueue<E> implements DynamicQueue<E> {
         return elements[front];
     }
 
-    // 当front到队尾时，回到队首
+    @Override
+    public E Rear() {
+        return elements[index(size - 1)];
+    }
+
     private int index(int index) {
 //        return (front + index) % elements.length;
 //        避免乘, 除, 模, 浮点等计算
         index += front;
+        if (index < 0) {
+            return index + elements.length;
+        }
+//        return index % elements.length;
         return index - ((index >= elements.length) ? elements.length : 0);
     }
 
